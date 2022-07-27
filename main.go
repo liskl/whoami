3db2d72ef872a500f4ada4a2815796e1e8ffea68
Loren Lisk <loren.lisk@liskl.com>
2022-07-27 09:47:44 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,46 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+  go func() {
+    for {
+			routeViewsRoot.Inc()
+    }
+  }()
+}
+
+var (
+
+        routeViewsRoot = promauto.NewCounter(prometheus.CounterOpts{
+                Name: "whoami_route_root_total",
+                Help: "The total number of views on /",
+        })
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
+	recordMetrics()
+	hostname, _ := os.Hostname()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
+		fmt.Fprintf(w, "I'm %s\n", hostname)
+	})
+	http.Handle("/metrics", promhttp.Handler())
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
